a1f859f486d4b3aefe6619259d801a45aa6a488d
hari141006 <harisowmya2006@gmail.com>
2026-09-08 22:03:03 +0530

    Constructors

--- OOPS/14_constructors/constructor.java
+++ OOPS/14_constructors/constructor.java
@@ -27,7 +27,7 @@ class Student {
 public class constructor {
 
     public static void main(String[] args) {
-        Student student = new Student();
+        Student  student = new Student();
 
     }
 }
